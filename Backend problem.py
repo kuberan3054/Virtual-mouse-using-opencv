5a27ff1e2459e55a7637f95f4f3ff47c12774302
kuberan3054 <140890521+kuberan3054@users.noreply.github.com>
2024-01-13 06:21:17 +0530

    Update Backend problem.py
    with stack overflow links
    
    
    Signed-off-by: kuberan3054 <140890521+kuberan3054@users.noreply.github.com>

--- Backend problem.py
+++ Backend problem.py
@@ -9,3 +9,7 @@ for gui in gui_env:
     except:
         continue
 print("Using:",matplotlib.get_backend())
+'''
+credits : https://stackoverflow.com/questions/3285193/how-to-change-matplotlib-backends/43015816#43015816
+for further clarification visit : https://stackoverflow.com/questions/73018899/how-to-fix-the-the-following-error-backend-tkagg-is-interactive-backend-turnin
+'''
